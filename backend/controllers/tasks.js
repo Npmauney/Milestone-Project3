501065de9b62161ff4a1e7796936377d6a3cb430
Npmauney <nathanielmauney@gmail.com>
2023-11-29 18:40:18 -0500

    fixed route and token

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -3,16 +3,9 @@ const db = require("../models")
 
 const { Task, User } = db
 
-router.post('/', async (req, res) => {
-    if (!req.body.pic) {
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-        req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    }
-    const place = await Place.create(req.body)
-    res.json(place)
+router.get('/', async (req, res) => {
+    const tasks = await Task.findAll()
+    res.json(tasks)
 })
+
+module.exports = router
