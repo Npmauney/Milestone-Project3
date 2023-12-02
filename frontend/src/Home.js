e652282ab5b0f0d2b4b76e30c95e0075d3e8a4f1
Npmauney <nathanielmauney@gmail.com>
2023-12-02 14:30:33 -0500

    added photo on home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,9 +4,9 @@ function Home() {
       <main>
         <h1>HOME</h1>
         <div >
-          <img height="300" width="500" src="http://localhost:5000/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+          <img height="300" width="500" src="http://localhost:5000/images/to-do-list.png" alt="to-do-list" />
           <div>
-            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+            Our crap
           </div>
         </div>
         <a href="/tasks">
